f57461922e1f10398143e5cbe4dd174f5c202806
Matt Bryan <mpbryan90@gmail.com>
2016-01-13 18:04:39 -0500

    set up RESTful api routes for items, reviews, and orders

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -6,7 +6,10 @@ module.exports = router;
 router.use('/members', require('./members'));
 // /api/categories
 router.use('/categories', require('./categories'));
-
+// /api/orders
+router.use('/reviews', require('./reviews'));
+// /api/reviews
+router.use('/orders', require('./orders'));
 // Make sure this is after all of
 // the registered routes!
 router.use(function (req, res) {
--- server/app/routes/reviews/index.js
+++ server/app/routes/reviews/index.js
@@ -1,11 +1,11 @@
 'use strict';
 var router = require('express').Router();
 var mongoose = require('mongoose');
-var Item = mongoose.model('Item');
+var Review = mongoose.model('Review');
 
 //read all
 router.get('/', function(req, res, next){
-	Item.find().exec()
+	Review.find().exec()
 	.then(function(results){
 		res.send(results);
 	});
@@ -13,35 +13,35 @@ router.get('/', function(req, res, next){
 
 //create
 router.post('/', function(req, res, next){
-	Item.create(req.body).exec()
+	Review.create(req.body).exec()
 	.then(function(result){
 		res.status(201).send(result);
 	});
 });
 
 //read one
-router.get('/:itemId', function(req, res, next){
-	Item.findOne({ _id: req.params.itemId } ).exec()
+router.get('/:reviewId', function(req, res, next){
+	Review.findOne({ _id: req.params.reviewId } ).exec()
 	.then(function(result){
 		res.send(result);
 	});
 });
 
 //update
-router.put('/:itemId', function(req, res, next){
-	Item.findOne({ _id: req.params.itemId } ).exec()
+router.put('/:reviewId', function(req, res, next){
+	Review.findOne({ _id: req.params.reviewId } ).exec()
 	.then(function(result){
 		result = req.body;
 		return result.save()
 	})
-	.then(function(updatedItem){
-		res.status(200).send(updatedItem);
+	.then(function(updatedReview){
+		res.status(200).send(updatedReview);
 	})
 });
 
 //delete
-router.delete('/:itemId', function(req, res, next){
-	Item.findOne({ _id: req.params.itemId } ).remove().exec()
+router.delete('/:reviewId', function(req, res, next){
+	Review.findOne({ _id: req.params.reviewId } ).remove().exec()
 	.then(function(result){
 		res.sendStatus(200);
 	});
